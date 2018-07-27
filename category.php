45562870189f07c924980058cd6123c91159d8a3
Malplaquet <boufflers.pierre@gmail.com>
2018-07-27 16:45:21 +0200

    améliorations visuelles de category.php et single.php

--- category.php
+++ category.php
@@ -3,17 +3,34 @@ get_header();
 ?>
 <div id="content" class="">
   <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <?php the_category('', 'single'); ?>
+      </div>
+    </div>
     <div class="row align-items-center">
-      <?php if (have_posts()) : while (have_posts()) : the_post();?>
-        <div class="col-4 thumbnails">
-          <?php the_post_thumbnail('miniature-article');?>
-          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-        </div>
-        <?php
-      endwhile;
-    endif;
-    ?>
-  </div>
+      <?php
+      $query = new WP_Query(array('posts_per_page' => 8, 'orderby' => 'date', 'order' => 'DESC' ));
+      if (have_posts()) : while (have_posts()) : the_post();?>
+      <div class="col-6 postcard_category">
+        <a class="category_content_article" href="<?php the_permalink(); ?>">
+          <div class="row">
+            <div class="col-6">
+              <img class="img-fluid" src="<?= esc_url(get_the_post_thumbnail_url($post->ID)) ?>" alt="">
+            </div>
+            <div class="col-6">
+              <span class="category_title_article"><?php the_title(); ?></span>
+            </div>
+          </div>
+          <?php the_excerpt(); ?>
+        </a>
+      </div>
+      <?php
+    endwhile;
+  endif;
+  ?>
+</div>
+</div>
 </div>
 <?php
 get_footer();
--- single.php
+++ single.php
@@ -2,43 +2,59 @@
 get_header();
 ?>
 <div id="content" class="">
-  <?php if (have_posts()) : while (have_posts()) : the_post();?>
-    <div class="container">
-      <div class="row">
-        <div class="col-9">
-          <div id="post-<?php the_ID(); ?>" class="post_title_article">
-            <h2><?php the_title(); ?></h2>
-          </div>
-          <div id="post-<?php the_ID(); ?>" class="post_content_article">
-            <?php the_content(); ?>
-          </div>
-          <?php
-        endwhile;
-      endif;
-      ?>
-    </div>
-    <div class="col-3 justify-content-center col_meme_sujet">
-      <p>Sur le même sujet :</p>
-      <!-- LOO-P avec articles à afficher selon étiquettes correspondantes -->
-      <?php
-      $query = new WP_Query( array('posts_per_page' => 4, 'orderby' => 'date', 'order' => 'DESC' ) );
-      if ($query->have_posts()) :
-        while ($query->have_posts()) :
-          $query->the_post();
-      ?>
-      <div class="row justify-content-center row_meme_sujet">
-        <div class="col-10 thumbnail_meme_sujet" style="background-image: url('<?= esc_url(get_the_post_thumbnail_url($post->ID)) ?>')">
-          <a href="<?php the_permalink(); ?>">
-            <p class="title_meme_sujet"><?php the_title(); ?></p>
-          </a>
+  <?php if (have_posts()) : while (have_posts()) : the_post();
+  $categories = get_the_category();
+  $categoriesName = [];
+  // var_dump( $categories);
+  if ($categories[0]->parent != 0) {
+    $categories = array_reverse($categories);
+  }
+  foreach ($categories as $category) {
+    $categoriesName[] = $category->name;
+  }
+  ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <p class="post_category_article"><?= implode( ' | ',$categoriesName); ?></p>
+        <div id="post-<?php the_ID(); ?>" class="post_title_article">
+          <?php the_title(); ?>
         </div>
-      </div>
-      <?php
+        <p class="post_date"><?php the_date('', 'Article publié le ', '.'); ?></p>
+        <div id="post-<?php the_ID(); ?>" class="post_content_article">
+          <?php the_content(); ?>
+        </div>
+        <?php
       endwhile;
-      endif;
-      ?>
-    </div>
+    endif;
+    ?>
   </div>
+  <div class="col-4 justify-content-center col_meme_sujet">
+    <div class="row">
+      <div class="col-10">
+        <p class="title_col_meme_sujet">Sur le même sujet :</p>
+      </div>
+    </div>
+    <!-- LOOP avec articles à afficher selon étiquettes correspondantes -->
+    <?php
+    $query = new WP_Query( array('posts_per_page' => 4, 'orderby' => 'date', 'order' => 'DESC' ) );
+    if ($query->have_posts()) :
+      while ($query->have_posts()) :
+        $query->the_post();
+        ?>
+        <div class="row justify-content-center row_meme_sujet">
+          <div class="col-10 thumbnail_meme_sujet" style="background-image: url('<?= esc_url(get_the_post_thumbnail_url($post->ID)) ?>')">
+            <a href="<?php the_permalink(); ?>">
+              <p class="title_thumbnail_meme_sujet"><?php the_title(); ?></p>
+            </a>
+          </div>
+        </div>
+        <?php
+      endwhile;
+    endif;
+    ?>
+  </div>
+</div>
 </div>
 </div>
 <?php
